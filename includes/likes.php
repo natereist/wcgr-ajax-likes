68f23f1e30804a0f6d5df0e83ebaebe0c0170def
Nate Reist <nate@mindutopia.com>
2014-08-14 13:31:58 -0400

    Step 1 : Class Stubbed out, metabox added and content filter added

--- includes/likes.php
+++ includes/likes.php
@@ -7,48 +7,16 @@
 			//set up hooks and filters
 			public function __construct(){
 			
-				add_action( 'wp_ajax_like_button_process', array( $this, 'process_like'), 10, 1 );
-				add_action( 'wp_ajax_nopriv_like_button_process', array( $this, 'process_like'), 10, 1 );
-				add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_like_scripts'), 10, 1 );
-				add_action( 'add_meta_boxes_post', array( $this, 'add_like_meta_box'), 10, 1 );
-				
-				add_filter( 'the_content', array( $this, 'like_post_template' ), 20, 1 );
 			}
 			
 			// enqueue scripts, styles and data
 			public function enqueue_like_scripts(){
-				wp_enqueue_script( 'jquery-cookie', plugins_url( '../js/jquery.cookie.js', __FILE__ ), array( 'jquery' ) );
-				wp_enqueue_script( 'wcgr_like_post', plugins_url( '../js/likes.js', __FILE__ ), array( 'jquery', 'jquery-cookie' ) );
-				wp_enqueue_style( 'wcgr_like_post_genericons', plugins_url( '../css/genericons.css', __FILE__ ) );
-				wp_enqueue_style( 'wcgr_like_post', plugins_url( '../css/likes.css', __FILE__ ), array( 'wcgr_like_post_genericons' ) );
 				
-				$object_for_js = array(
-					'ajax_url' => admin_url( '/admin-ajax.php' ),
-					'nonce' => wp_create_nonce('like_post')
-				);
-				
-				wp_localize_script( 'wcgr_like_post', 'ajax_data', $object_for_js );
 			}
 			
 			// ajax callback
 			public function process_like(){
-				if ( isset( $_POST['like_nonce'] ) ) {
-					$nonce = $_POST['like_nonce'];
-					
-					if ( wp_verify_nonce($nonce, 'like_post') ) {
-						
-						// process the like here
-						$post_to_like = $_POST['post_to_like'];
-						$count = get_post_meta( $post_to_like, 'like_count', 1 );
-						
-						$count += 1;
-						
-						update_post_meta( $post_to_like, 'like_count', $count );
-						
-						wp_send_json( array( 'like_count' => $count, 'post_liked' => $post_to_like ) );
-						
-					}
-				}
+				
 			}
 			
 			// out puts the meta box content
--- js/likes.js
+++ js/likes.js
@@ -1,34 +1,3 @@
 jQuery( document ).ready(function( $ ){
-	$('a.like-this-post').each(function (){
-		
-		var post = $(this).data( 'post_id' );
-		
-		var liked_cookie = $.cookie( 'liked_post_' + post );
-		if( typeof liked_cookie != 'undefined' ){
-			$(this).text( 'Liked' ).addClass( 'liked' );;
-		}
-	})
-	$('body').on('click', 'a.like-this-post', function(){
-		var post = $(this).data( 'post_id' );
-		
-		var liked_cookie = $.cookie( 'liked_post_' + post );
-		
-		if( typeof liked_cookie == 'undefined' ){
-			var data = {
-				post_to_like : post,
-				action : 'like_button_process',
-				like_nonce : ajax_data.nonce
-			}
-			
-			$.cookie( 'liked_post_' + post, true, { expires: 365, path: '/' });
-			
-			$.post( ajax_data.ajax_url, data, function( response ){
-				$('span#count-' + response.post_liked ).text( response.like_count );
-				$('a#like-' + response.post_liked ).text( 'Liked' ).addClass( 'liked' );
-			}, 'JSON' );
-		}
-		else{
-			console.log( 'You already like this post.' );
-		}
-	});
+	
 });
